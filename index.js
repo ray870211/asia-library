2f0415507c609cc96c98a8bbe1467d40ae425f17
ray <ray870211@gmail.com>
2021-10-04 12:21:28 +0800

    update

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var context = canvas.getContext("2d");
 var img = document.querySelector("img");
 var imgData = null;
 var localMediaStream = null;
+var user_data;
 navigator.getUserMedia =
   navigator.getUserMedia ||
   navigator.webkitGetUserMedia ||
@@ -39,15 +40,32 @@ function snap() {
 }
 
 $("#register").click(function () {
-  sendToServer("./face_detection.py/reg", imgData);
+  user_data = {
+    name: document.getElementsByTagName("input").name.value,
+    account_id: document.getElementsByTagName("input").account_id.value,
+    u_id: document.getElementsByTagName("input").u_id.value,
+    myclass: document.getElementsByTagName("input").myclass.value,
+    gender: document.getElementsByTagName("select").gender.value,
+    imaData: imgData,
+  };
+  sendToServer("http://10.96.26.249:5001/reg");
 });
 
-function sendToServer(url, dataURL) {
+function sendToServer(url) {
   $.ajax({
     type: "POST",
     url: url,
+    dataType: "jsonp",
+    heads: {
+      "content-type": "application/x-www-form-url-lencoded",
+    },
     data: {
-      imgBase64: dataURL,
+      name: user_data.name,
+      account_id: user_data.account_id,
+      u_id: user_data.u_id,
+      myclass: user_data.myclass,
+      gender: user_data.gender,
+      imaData: user_data.imgData,
     },
     success: function () {
       print("work");
